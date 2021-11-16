1083ca9ec917e97e10c16550360281a19704c5c9
Alexandr Marchenko <alexandrm@rabota.ua>
2021-11-16 13:22:59 +0200

    wildcard

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import core from '@actions/core';
-import github from '@actions/github';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 try {
-  const version = core.getInput('version');
+  const version = core.getInput('version') || '1.0';
   if (!version || !version.match(/\d+\.\d+/)) {
     core.setFailed(`Unexpected version "${version}" must be something like "2.1"`);
   }
@@ -10,7 +10,6 @@ try {
   core.exportVariable('GITHUB_SHA_SHORT', (github.context.eventName === 'pull_request' ? github.context.payload.pull_request?.head.sha : github.context.sha).substring(0, 7))
   core.exportVariable('GITHUB_BRANCH_SLUG', process.env.GITHUB_HEAD_REF?.split('/')?.pop() || github.context.ref.split('/').pop())
   core.exportVariable('VERSION', `${version}.${process.env.GITHUB_RUN_NUMBER}-${process.env.GITHUB_SHA_SHORT}-${process.env.GITHUB_BRANCH_SLUG}`)
-
 } catch (error) {
   core.setFailed((error as Error).message);
 }
