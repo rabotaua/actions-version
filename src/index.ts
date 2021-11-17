8b39213b8d992df932c0287224862e2f791318d6
Alexandr Marchenko <alexandrm@rabota.ua>
2021-11-17 18:37:41 +0200

    prettier

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,10 @@ try {
     core.setFailed(`Unexpected version "${version}" must be something like "2.1"`);
   }
 
-  exportVariable(
-    "GITHUB_SHA_SHORT",
-    (github.context.eventName === "pull_request" ? github.context.payload.pull_request?.head.sha : github.context.sha).substring(0, 7)
-  );
+  const commit = github.context.eventName === "pull_request" ? github.context.payload.pull_request?.head.sha : github.context.sha;
+  const sha = commit.substring(0, 7);
+
+  exportVariable("GITHUB_SHA_SHORT", sha);
 
   console.log({
     env: process.env.GITHUB_HEAD_REF,
@@ -24,10 +24,7 @@ try {
 
   exportVariable("GITHUB_BRANCH_SLUG", (process.env.GITHUB_HEAD_REF?.split("/")?.pop() || github.context.ref.split("/").pop())!);
 
-  exportVariable(
-    "VERSION",
-    `${version}.${process.env.GITHUB_RUN_NUMBER}-${process.env.GITHUB_SHA_SHORT}-${process.env.GITHUB_BRANCH_SLUG}`
-  );
+  exportVariable("VERSION", `${version}.${process.env.GITHUB_RUN_NUMBER}-${sha}-${process.env.GITHUB_BRANCH_SLUG}`);
 } catch (error) {
   core.setFailed((error as Error).message);
 }
